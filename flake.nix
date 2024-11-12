eb7d92ba4914c7e8addfa23b49424c8a3dd0e086
Mastoca <121066551+mastoca@users.noreply.github.com>
2024-11-12 14:51:09 -0500

    update to 0.4.1

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description =
-    "ollama: Get up and running with Llama 2, Mistral, and other large language models locally";
+  description = "ollama: Get up and running with Llama 2, Mistral, and other large language models locally";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -10,32 +9,34 @@
     };
   };
 
-  outputs = { nixpkgs, nixpkgs-unfree, ... }:
-    let
-      inherit (nixpkgs) lib;
+  outputs = {
+    nixpkgs,
+    nixpkgs-unfree,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
 
-      forAllSystems = systems: buildPackages:
-        lib.genAttrs systems (system:
-          buildPackages nixpkgs-unfree.legacyPackages.${system});
+    forAllSystems = systems: buildPackages:
+      lib.genAttrs systems (system:
+        buildPackages nixpkgs-unfree.legacyPackages.${system});
 
-      buildPackages = systems: packageOverrides:
-        forAllSystems systems (pkgs:
-          builtins.mapAttrs
-            (_: pkgs.callPackage ./package.nix)
-            (packageOverrides pkgs));
+    buildPackages = systems: packageOverrides:
+      forAllSystems systems (pkgs:
+        builtins.mapAttrs
+        (_: pkgs.callPackage ./package.nix)
+        (packageOverrides pkgs));
 
-      unixPackages = buildPackages lib.platforms.unix (pkgs: {
-        default = { };
-      });
+    unixPackages = buildPackages lib.platforms.unix (pkgs: {
+      default = {};
+    });
 
-      linuxPackages = buildPackages lib.platforms.linux (pkgs: {
-        default = { };
-        rocm = { acceleration = "rocm"; };
-        cuda = { acceleration = "cuda"; };
-        cpu = { acceleration = false; };
-      });
-    in
-    {
-      packages = unixPackages // linuxPackages;
-    };
+    linuxPackages = buildPackages lib.platforms.linux (pkgs: {
+      default = {};
+      rocm = {acceleration = "rocm";};
+      cuda = {acceleration = "cuda";};
+      cpu = {acceleration = false;};
+    });
+  in {
+    packages = unixPackages // linuxPackages;
+  };
 }
